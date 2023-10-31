0821030bd536d935a03f5a93e2f6adb19702f440
Naufal Gajendra Ardiputra <53498520+naufal025@users.noreply.github.com>
2023-10-31 17:58:47 +0700

    Add files via upload

---
+++ jobsheet10/PenambahanBuku.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class PenambahanBuku {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+
+        String[] namaBuku = new String[100];
+        int[] kodeBuku = new int[100];
+        System.out.println("Masukkan Jumlah Buku : ");
+        int jumlah = sc.nextInt();
+        for(int i =0; i<jumlah; i++){
+            System.out.println("Masukkan Nama Buku : ");
+            sc.nextLine();
+            namaBuku[i] = sc.nextLine();
+            System.out.println("Masukkan Kode Buku : ");
+            kodeBuku[i] = sc.nextInt();
+        }
+
+        for (int i=0; i < jumlah; i++) {
+            System.out.println("NAma Buku : " + namaBuku[i] + ", Kode Buku : " + kodeBuku[i]);   
+        
+        
+        }
+    }
+    
+}
